f235e298c71962ac20324ccc1331300a985aba66
Hikmet Melik Fırat <hikmetmelik@gmail.com>
2024-10-26 23:31:22 +0300

    authentication completed but not working, need fix

--- app/login/LoginForm.tsx
+++ app/login/LoginForm.tsx
@@ -1,13 +1,35 @@
+"use client";
+
 import styles from "@/app/login/login.module.css";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
+import { login } from "./action";
+
 export default function LoginForm() {
+	const [state, loginAction] = useActionState(login, undefined);
 	return (
 		<div className={styles.loginContainer}>
-			<form>
+			<form action={loginAction}>
 				<h1>Login to app</h1>
-				<input type="email" placeholder="Email" />
-				<input type="password" placeholder="Password" />
-				<button>Login </button>
+				<input type="email" placeholder="Email" id="email" />
+				{state?.errors?.email && (
+					<p className={styles.errorText}>{state.errors.email}</p>
+				)}
+				<input type="password" placeholder="Password" id="password" />
+				{state?.errors?.password && (
+					<p className={styles.errorText}>{state.errors.password}</p>
+				)}
+				<SubmitButton />
 			</form>
 		</div>
+	);
+}
+function SubmitButton() {
+	const { pending } = useFormStatus();
+
+	return (
+		<button disabled={pending} type="submit">
+			Login
+		</button>
 	);
 }
